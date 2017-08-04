61a4483cf3eb008b3c2f196486dea423034e5c18
Milivoje <milivojeivic422@gmail.com>
2017-08-04 19:03:58 +0200

    Secund -last commit

--- js/main.js
+++ js/main.js
@@ -23,3 +23,33 @@ $('.portfolio-item').mouseenter(function(){
 $('.portfolio-item').mouseleave(function(){
   $(this).removeClass('active-box');
 });
+
+
+$('#you form').mouseenter(function(){
+  $('.form_buttons').addClass('form_focused');
+});
+$('#you form').mouseleave(function(){
+  $('.form_buttons').removeClass('form_focused');
+});
+
+$(document).ready(function () {
+    var sildeNum = $('.page').length,
+        wrapperWidth = 100 * sildeNum,
+        slideWidth = 100/sildeNum;
+
+    $('.wrapper').width(wrapperWidth + '%'); 
+    $('.page').width(slideWidth + '%');
+    $('a.scrollitem').click(function(){
+        $('a.scrollitem').removeClass('selected');
+        $(this).addClass('selected');
+        var slideNumber = $($(this).attr('href')).index('.page'),
+            margin = slideNumber * -100 + '%';
+        $('.wrapper').animate({marginLeft: margin},800);
+        return false;
+    });
+});
+
+$('.nav-item a').click(function(){
+    $('.nav-item').removeClass('active');
+    $(this).parent().addClass('active');
+});
